c825cc55d91a6b6cfe806743b0cd38428c7c227d
xxo <xxoxec@gmail.com>
2024-07-05 01:19:45 +0900

    Feat: appconfig를 통해 관심사를 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,23 +1,18 @@
 package hello.core.order;
 
 import hello.core.discount.DisCountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    /*
-    인터페이스에만 의존하도록 설계와 코드를 변경
-    구현체가 없어서 코드 실행 불가
-    실제 실행해보면 NPE발생
-    이 문제를 해결하려면 누군가 클라이언트인
-    orderServiceImpl에 Discountpolicy의 구현 객체를 대신 생성하고 주입해주어야 한다.
-     */
-    private DisCountPolicy disCountPolicy;
+    private final MemberRepository memberRepository;
+    private final DisCountPolicy disCountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DisCountPolicy disCountPolicy) {
+        this.memberRepository = memberRepository;
+        this.disCountPolicy = disCountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
